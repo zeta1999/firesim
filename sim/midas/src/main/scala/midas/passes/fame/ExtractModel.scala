97e5606fe3e1f5ef07a4991d332d18645cea1e4b
Albert Magyar <albert.magyar@gmail.com>
2020-05-24 10:13:01 +0000

    More WIP

--- sim/midas/src/main/scala/midas/passes/fame/ExtractModel.scala
+++ sim/midas/src/main/scala/midas/passes/fame/ExtractModel.scala
@@ -13,6 +13,7 @@ import annotations.{InstanceTarget, Annotation, SingleTargetAnnotation}
 
 import midas.targetutils.FirrtlFAMEModelAnnotation
 
+import scala.annotation.tailrec
 import scala.collection.mutable
 import mutable.{LinkedHashSet, LinkedHashMap}
 
@@ -20,20 +21,21 @@ class ExtractModel extends Transform {
   def inputForm = HighForm
   def outputForm = HighForm
 
-  def promoteModels(state: CircuitState): CircuitState = {
+  @tailrec
+  private def promoteModels(state: CircuitState): CircuitState = {
     val fmAnns = state.annotations.collect({
       case ann: FirrtlFAMEModelAnnotation => ann.target.module -> ann
     }).toMap
     // Pick by order of parent in linearization -- don't pick children of main
     val modOrder = (new InstanceGraph(state.circuit)).moduleOrder.filterNot(_.name == state.circuit.main)
-    println(modOrder.map(_.name))
     val topModelAnn = modOrder.collectFirst(Function.unlift(dm => fmAnns.get(dm.name)))
-    topModelAnn.map({
-      case FirrtlFAMEModelAnnotation(it) =>
+    topModelAnn match {
+      case None => state
+      case Some(FirrtlFAMEModelAnnotation(it)) =>
         val anns = PromoteSubmoduleAnnotation(it) +: state.annotations
-        println(s"Promoting ${it.module}.${it.instance} of ${it.ofModule}")
-        promoteModels((new PromoteSubmodule).runTransform(state.copy(annotations = anns)))
-    }).getOrElse(state)
+        val nextPromoted = (new PromoteSubmodule).runTransform(state.copy(annotations = anns))
+        promoteModels(nextPromoted)
+    }
   }
 
   override def execute(state: CircuitState): CircuitState = {
